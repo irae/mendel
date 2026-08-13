ce1b467840c055f01351bc1a4e9d55726e6b756a
Irae Carvalho <irae@irae.pro.br>
2026-08-13 15:33:45 -0300

    fix(test): restore root app-samples fixtures for legacy suite
    test/app-samples was left empty when its contents moved into
    packages/mendel-core/test/app-samples, breaking module/file
    resolution for the root legacy tests that still reference the
    original path.

---
+++ test/app-samples/1/app/number-list.js
@@ -0,0 +1,10 @@
+/* Copyright 2015, Yahoo Inc.
+   Copyrights licensed under the MIT License.
+   See the accompanying LICENSE file for terms. */
+
+var oneNumber = require('./some-number');
+
+module.exports = function () {
+    var a = [oneNumber()];
+    return Array.isArray(a) ? a : [];
+};
---
+++ test/app-samples/1/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -d build/ ]
+then
+    rm -rf build/
+fi
+if [ -d build-requirify/ ]
+then
+    rm -rf build-requirify/
+fi
+
+mkdir -p build
+mkdir -p build-requirify
+
+../../../node_modules/.bin/browserify \
+    app/index.js -p ../../../packages/mendel-browserify \
+    -p  ../../../packages/mendel-requirify \
+    -o build/app.js
