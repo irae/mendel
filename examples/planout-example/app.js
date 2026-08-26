b441e68fb63dbada439f45606707328bd8a56f8f
Irae Carvalho <irae@irae.pro.br>
2026-08-25 22:24:54 -0300

    fix(planout-example): replace uuid with crypto.randomUUID()

--- examples/planout-example/app.js
+++ examples/planout-example/app.js
@@ -6,7 +6,7 @@ var ReactDOMServer = require('react-dom/server');
 var express = require('express');
 var logger = require('morgan');
 var MendelMiddleware = require('mendel-middleware');
-var { v4: uuidv4 } = require('uuid');
+var { randomUUID: uuidv4 } = require('crypto');
 var PlanoutAssignment = require('./PlanoutAssignment');
 var cookieParser = require('cookie-parser');
 if (process.env.NODE_ENV !== 'production') {
