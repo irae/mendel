55a381286e520edb8e8a1d5b5ec4289edb3631bb
Irae Carvalho <irae@irae.pro.br>
2026-08-28 18:05:57 -0300

    test(mendel-deps): use native globSync

--- packages/mendel-deps/test/js.js
+++ packages/mendel-deps/test/js.js
@@ -1,7 +1,6 @@
 const test = require('tap').test;
 const deps = require('../');
-const { readFileSync } = require('fs');
-const { globSync } = require('glob');
+const { globSync, readFileSync } = require('fs');
 const path = require('path');
 const Resolver = require('../../mendel-resolver');
 
