92e39aa10b1ed75bea8f11e1976fdfc7849b091e
Irae Carvalho <irae@irae.pro.br>
2026-08-28 17:20:45 -0300

    fix(mendel-development): use fs.mkdtempSync instead of tmp
    Node's fs.mkdtempSync creates a disposable temp directory natively.
    validate-manifest.js keeps leaving its debug file behind on purpose,
    for a developer to inspect after a failed build; the test adds its
    own cleanup on exit, since tmp used to do that automatically.

--- packages/mendel-development/test/post-process-manifest.js
+++ packages/mendel-development/test/post-process-manifest.js
@@ -4,12 +4,15 @@
 
 var test = require('tap').test;
 var fs = require('fs');
+var os = require('os');
 var path = require('path');
-var tmp = require('tmp');
 
 // Since this file re-writes stuff, lets work on a copy
 var realSamples = path.join(__dirname, './manifest-samples/');
-var copySamples = tmp.dirSync().name;
+var copySamples = fs.mkdtempSync(path.join(os.tmpdir(), 'mendel-'));
+process.on('exit', function () {
+    fs.rmSync(copySamples, { recursive: true, force: true });
+});
 
 var postProcessManifests = require('../post-process-manifest');
 
