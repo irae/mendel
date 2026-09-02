cea39344cbb26f77a80421e364641a7fa1922050
Irae Carvalho <irae@irae.pro.br>
2026-09-02 16:14:24 -0300

    fix: replace tmp with fs.mkdtempSync()

--- packages/mendel-manifest-uglify/test/manifest-uglify.js
+++ packages/mendel-manifest-uglify/test/manifest-uglify.js
@@ -5,14 +5,14 @@
 var tap = require('tap');
 var fs = require('fs');
 var path = require('path');
-var tmp = require('tmp');
+var os = require('os');
 
 // Since this file re-writes stuff, lets work on a copy
 var realSamples = path.join(
     __dirname,
     '../../mendel-development/test/manifest-samples'
 );
-var copySamples = tmp.dirSync().name;
+var copySamples = fs.mkdtempSync(path.join(os.tmpdir(), 'mendel-'));
 
 var postProcessManifests = require('../../mendel-development/post-process-manifest');
 var uglify = require('../manifest-uglify');
@@ -22,6 +22,7 @@ var simpleCompressionSize;
 tap.test('mendel-manifest-uglify compress', function (t) {
     copyRecursiveSync(realSamples, copySamples);
     t.plan(1);
+    t.teardown(() => fs.rmSync(copySamples, { recursive: true, force: true }));
 
     postProcessManifests(
         {
