573e19b85a113cb3655da8943eb988b6b56215c7
Irae Carvalho <irae@irae.pro.br>
2026-09-02 04:03:37 -0300

    chore(mendel-development): replace glob with fs.promises.glob

--- packages/mendel-development/apply-extra-options.js
+++ packages/mendel-development/apply-extra-options.js
@@ -3,7 +3,7 @@
    See the accompanying LICENSE file for terms. */
 
 var path = require('path');
-var { glob } = require('glob');
+var { glob } = require('fs').promises;
 
 module.exports = applyExtraOptions;
 
@@ -16,7 +16,7 @@ function applyExtraOptions(b, options) {
         .filter(Boolean)
         .forEach(function (i) {
             b._pending++;
-            glob(i)
+            Array.fromAsync(glob(i))
                 .then(function (files) {
                     if (files.length === 0) {
                         b.ignore(i);
@@ -39,7 +39,7 @@ function applyExtraOptions(b, options) {
             b.exclude(u);
 
             b._pending++;
-            glob(u)
+            Array.fromAsync(glob(u))
                 .then(function (files) {
                     files.forEach(function (file) {
                         b.exclude(file);
@@ -61,7 +61,7 @@ function applyExtraOptions(b, options) {
             } else if (/\*/.test(x)) {
                 b.external(x);
                 b._pending++;
-                glob(x)
+                Array.fromAsync(glob(x))
                     .then(function (files) {
                         files.forEach(function (file) {
                             add(file, {});
