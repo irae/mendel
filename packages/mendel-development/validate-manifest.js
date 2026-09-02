6d8d6c9b232777fb6223e274a6aa3162b3db862a
Irae Carvalho <irae@irae.pro.br>
2026-09-02 00:39:09 -0300

    chore(mendel-development): replace tmp with fs.mkdtempSync

--- packages/mendel-development/validate-manifest.js
+++ packages/mendel-development/validate-manifest.js
@@ -4,7 +4,7 @@
 
 var path = require('path');
 var fs = require('fs');
-var tmp = require('tmp');
+var os = require('os');
 
 module.exports = validateManifest;
 
@@ -52,7 +52,7 @@ function validateManifest(manifest, originalPath, stepName) {
             console.log('  ' + log);
         });
 
-        var tempDir = tmp.dirSync().name;
+        var tempDir = fs.mkdtempSync(path.join(os.tmpdir(), 'mendel-debug-'));
         var filename = 'debug.' + path.parse(originalPath).base;
         var destination = path.resolve(tempDir, filename);
         fs.writeFileSync(destination, JSON.stringify(manifest, null, 2));
