cc134f12f2affb90be7cb9da8592a8add88edc84
Irae Carvalho <irae@irae.pro.br>
2026-09-02 15:52:13 -0300

    fix: replace tmp with fs.mkdtempSync in mendel-development

--- packages/mendel-development/test/post-process-manifest.js
+++ packages/mendel-development/test/post-process-manifest.js
@@ -5,11 +5,11 @@
 var test = require('tap').test;
 var fs = require('fs');
 var path = require('path');
-var tmp = require('tmp');
+var os = require('os');
 
 // Since this file re-writes stuff, lets work on a copy
 var realSamples = path.join(__dirname, './manifest-samples/');
-var copySamples = tmp.dirSync().name;
+var copySamples = fs.mkdtempSync(path.join(os.tmpdir(), 'mendel-'));
 
 var postProcessManifests = require('../post-process-manifest');
 
@@ -175,3 +175,7 @@ function copyRecursiveSync(src, dest) {
         fs.writeFileSync(dest, fs.readFileSync(src));
     }
 }
+
+process.on('exit', () => {
+    fs.rmSync(copySamples, { recursive: true, force: true });
+});
