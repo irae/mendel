2e0926dea4472362aa6594f2f7a44a67c252d787
Irae Carvalho <irae@irae.pro.br>
2026-09-02 00:39:20 -0300

    chore(mendel-manifest-extract-bundles): replace tmp with fs.mkdtempSync

--- packages/mendel-manifest-extract-bundles/test/manifest-extract.js
+++ packages/mendel-manifest-extract-bundles/test/manifest-extract.js
@@ -5,14 +5,14 @@
 var test = require('tap').test;
 var fs = require('fs');
 var path = require('path');
-var tmp = require('tmp');
+var os = require('os');
 
 // Since this file re-writes stuff, lets work on a copy
 var realSamples = path.join(
     __dirname,
     '../../mendel-development/test/manifest-samples/'
 );
-var copySamples = tmp.dirSync().name;
+var copySamples = fs.mkdtempSync(path.join(os.tmpdir(), 'mendel-test-'));
 
 var postProcessManifests = require('../../mendel-development/post-process-manifest');
 var extract = require('../manifest-extract');
