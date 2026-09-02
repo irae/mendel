bd0ebd45fa6ffbd70937a809c273294691457e4d
Irae Carvalho <irae@irae.pro.br>
2026-09-02 15:52:17 -0300

    fix: replace tmp with fs.mkdtempSync in mendel-manifest-extract-bundles

--- packages/mendel-manifest-extract-bundles/test/manifest-extract.js
+++ packages/mendel-manifest-extract-bundles/test/manifest-extract.js
@@ -5,14 +5,14 @@
 var test = require('tap').test;
 var fs = require('fs');
 var path = require('path');
-var tmp = require('tmp');
+var os = require('os');
 
 // Since this file re-writes stuff, lets work on a copy
 var realSamples = path.join(
     __dirname,
     '../../mendel-development/test/manifest-samples/'
 );
-var copySamples = tmp.dirSync().name;
+var copySamples = fs.mkdtempSync(path.join(os.tmpdir(), 'mendel-'));
 
 var postProcessManifests = require('../../mendel-development/post-process-manifest');
 var extract = require('../manifest-extract');
@@ -89,3 +89,7 @@ function copyRecursiveSync(src, dest) {
         fs.writeFileSync(dest, fs.readFileSync(src));
     }
 }
+
+process.on('exit', () => {
+    fs.rmSync(copySamples, { recursive: true, force: true });
+});
