7ca0899a6f944c8aa0c31bcbfb271770f9ab5704
Irae Carvalho <irae@irae.pro.br>
2026-09-02 22:33:55 -0300

    chore: replace glob with fs.globSync in mendel-deps tests

--- packages/mendel-deps/test/css.js
+++ packages/mendel-deps/test/css.js
@@ -1,7 +1,7 @@
 const test = require('tap').test;
 const deps = require('../');
 const { readFileSync } = require('fs');
-const { globSync } = require('glob');
+const { globSync } = require('node:fs');
 const path = require('path');
 const Resolver = require('../../mendel-resolver');
 
