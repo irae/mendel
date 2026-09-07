2b160a8e734eaa2e53c378d4bfaf32fb5cacc996
Irae Carvalho <irae@irae.pro.br>
2026-09-06 22:45:08 -0300

    chore(mendel-pipeline): replace shasum, tmp, and chalk with native Node.js

--- packages/mendel-pipeline/src/helpers/analytics/cli-printer.js
+++ packages/mendel-pipeline/src/helpers/analytics/cli-printer.js
@@ -1,5 +1,5 @@
 const BasePrinter = require('./printer');
-const { default: chalk } = require('chalk');
+const { styleText } = require('node:util');
 const { default: prettyMs } = require('pretty-ms');
 const { default: figure } = require('figures');
 
@@ -7,8 +7,9 @@ function getBarText(percent, maxBarSize) {
     maxBarSize = Math.max(0, maxBarSize);
     const barNumber = Math.max(Math.ceil((percent / 100) * maxBarSize), 1);
     return (
-        chalk.blue(new Array(barNumber + 1).join(figure.square)) +
-        chalk.blue.dim(
+        styleText('blue', new Array(barNumber + 1).join(figure.square)) +
+        styleText(
+            ['blue', 'dim'],
             new Array(Math.max(0, maxBarSize - barNumber + 1)).join(
                 figure.square
             )
@@ -28,7 +29,6 @@ class CliPrinter extends BasePrinter {
     constructor(options = {}) {
         super(options);
 
-        chalk.level = options.enableColor !== false ? 3 : 0;
         this.processStart = Date.now();
 
         this.nameMaxLen = options.nameMaxLen || 30;
@@ -57,7 +57,7 @@ class CliPrinter extends BasePrinter {
 
                 console.log(
                     new Array(indentation + 1).join('  ') +
-                        chalk.underline(groupedName)
+                        styleText('underline', groupedName)
                 );
                 this._print(dataPart, dimensions.slice(1), indentation + 1);
             });
@@ -131,7 +131,8 @@ class CliPrinter extends BasePrinter {
 
     print(data) {
         console.log(
-            chalk.bgWhite.black(
+            styleText(
+                ['bgWhite', 'black'],
                 padRight(
                     ' Sorted by grouping (aggregate of all thread)',
                     process.stdout.columns || 80
@@ -141,14 +142,16 @@ class CliPrinter extends BasePrinter {
         this._print(data, [1]);
 
         console.log(
-            chalk.bgWhite.black(
+            styleText(
+                ['bgWhite', 'black'],
                 padRight(' Sorted by subgroup', process.stdout.columns || 80)
             )
         );
         this._print(data, [1, 2]);
 
         console.log(
-            chalk.bgWhite.black(
+            styleText(
+                ['bgWhite', 'black'],
                 padRight(' Sorted by pid', process.stdout.columns || 80)
             )
         );
@@ -158,8 +161,10 @@ class CliPrinter extends BasePrinter {
             new Array((process.stdout.columns || 80) + 1).join(figure.line)
         );
         console.log(
-            chalk.white(
-                `Process finished in ${chalk.bold(
+            styleText(
+                'white',
+                `Process finished in ${styleText(
+                    'bold',
                     prettyMs(Date.now() - this.processStart)
                 )}.`
             )
