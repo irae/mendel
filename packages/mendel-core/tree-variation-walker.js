d1cb0802cf3fed8a1896c4ccc61f8e8049683083
Irae Carvalho <irae@irae.pro.br>
2026-09-15 04:28:19 -0300

    chore(mendel-core, mendel-config): replace xtend with Object.assign

--- packages/mendel-core/tree-variation-walker.js
+++ packages/mendel-core/tree-variation-walker.js
@@ -5,7 +5,6 @@
 
 var debug = require('debug')('mendel:tree-variation-walker');
 var util = require('util');
-var xtend = require('xtend');
 
 var MendelWalker = require('./tree-walker');
 
@@ -58,7 +57,7 @@ MendelVariationWalker.prototype._resolveBranch = function (module) {
 };
 
 MendelVariationWalker.prototype.found = function () {
-    return xtend(MendelWalker.prototype.found.call(this), {
+    return Object.assign({}, MendelWalker.prototype.found.call(this), {
         conflicts: this.conflicts,
         conflictList: this.conflictList,
     });
