2b6480757d3bb0ca5ed721c21d1a511f72971f4b
Irae Carvalho <irae@irae.pro.br>
2026-09-19 00:08:08 -0300

    fix(mendel-development): replace glob with fs.glob

--- packages/mendel-development/apply-extra-options.js
+++ packages/mendel-development/apply-extra-options.js
@@ -3,20 +3,28 @@
    See the accompanying LICENSE file for terms. */
 
 var path = require('path');
-var { glob } = require('glob');
+var fs = require('fs');
 
 module.exports = applyExtraOptions;
 
 // Browserify's bundle() waits on `_ready` while `_pending` async work finishes.
 // Honor that handshake for ignore/exclude/external globs so multi-bundle and
 // extract-style pipelines do not race past unfinished path resolution.
+function globFiles(pattern) {
+    return new Promise(function (resolve, reject) {
+        fs.glob(pattern, function (err, files) {
+            if (err) return reject(err);
+            resolve(files);
+        });
+    });
+}
 function applyExtraOptions(b, options) {
     []
         .concat(options.ignore)
         .filter(Boolean)
         .forEach(function (i) {
             b._pending++;
-            glob(i)
+            globFiles(i)
                 .then(function (files) {
                     if (files.length === 0) {
                         b.ignore(i);
@@ -39,7 +47,7 @@ function applyExtraOptions(b, options) {
             b.exclude(u);
 
             b._pending++;
-            glob(u)
+            globFiles(u)
                 .then(function (files) {
                     files.forEach(function (file) {
                         b.exclude(file);
@@ -61,7 +69,7 @@ function applyExtraOptions(b, options) {
             } else if (/\*/.test(x)) {
                 b.external(x);
                 b._pending++;
-                glob(x)
+                globFiles(x)
                     .then(function (files) {
                         files.forEach(function (file) {
                             add(file, {});
